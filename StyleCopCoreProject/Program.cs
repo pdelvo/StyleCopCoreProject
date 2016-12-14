5ab8e6ac85d9e08fed1c18f35e142e21db1b4248
Dennis Fischer <fischer_dennis@live.de>
2016-12-14 20:57:25 +0100

    Initial commit

---
+++ StyleCopCoreProject/Program.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="Program.cs" company="Stylecop Analyzers Company">
+// Copyright (c) Stylecop Analyzers Company. All rights reserved.
+// </copyright>
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
